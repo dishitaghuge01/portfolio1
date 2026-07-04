a97253617e80acd52600d0757e8977dc080380c3
Dishita <ghugedishita@gmail.com>
2026-07-04 17:54:06 +0530

    wire mobilebook into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { BookProvider } from './contexts/BookContext';
 import { spreadsMeta } from './data/spreads';
+import { useIsMobile } from './hooks/useIsMobile';
 import Book from './components/book/Book';
+import MobileBook from './components/book/MobileBook';
 import LampChain from './components/book/LampChain';
 import type { Theme } from './types/index';
 
 const App: React.FC = () => {
   const [theme, setTheme] = useState<Theme>('dark');
+  const isMobile = useIsMobile();
 
   // Sync theme to document root so CSS token selectors fire
   useEffect(() => {
@@ -19,7 +22,7 @@ const App: React.FC = () => {
   return (
     <>
       <BookProvider totalSpreads={spreadsMeta.length}>
-        <Book />
+        {isMobile ? <MobileBook /> : <Book />}
       </BookProvider>
 
       <LampChain theme={theme} onToggle={toggleTheme} />
